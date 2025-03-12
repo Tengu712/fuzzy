da05c9e74f5e4c5dfde8b12e1899b2e825b88800
Tengu712 <genreihoutengu@gmail.com>
2025-03-12 22:53:41 +0900

    feat: run empty code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+mod parser;
+
+type EError = Box<dyn std::error::Error>;
+
+fn run() -> Result<(), EError> {
+    use std::{env, fs::File};
+
+    let args = env::args().skip(1).collect::<Vec<String>>();
+
+    if args.len() > 2 {
+        return Err("too much arguments passed.".to_string().into());
+    }
+    if args.is_empty() {
+        // TODO: implement REPL.
+        return Ok(());
+    }
+
+    // TODO: get tokens and pass them to evaluator.
+    let _ = parser::parse(File::open(&args[0])?);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        eprintln!("error: {}", e);
+        std::process::exit(1);
+    }
 }
