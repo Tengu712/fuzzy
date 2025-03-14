b6ba59d08d423c1f139d514dbdaede3590508ef8
Tengu712 <genreihoutengu@gmail.com>
2025-03-15 04:06:17 +0900

    refactor: lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,5 +1,9 @@
 use crate::EError;
-use std::io::{BufRead, BufReader, Read};
+use std::{
+    io::{BufRead, BufReader, Read},
+    iter::Peekable,
+    str::CharIndices,
+};
 
 mod number;
 
@@ -18,6 +22,14 @@ pub enum Token {
     U128(u128),
     Symbol(String),
 }
+impl Token {
+    fn from(s: String) -> Self {
+        match s.as_str() {
+            "." => Self::Dot,
+            _ => number::lex(&s).unwrap_or(Self::Symbol(s)),
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct TokenView {
@@ -25,101 +37,69 @@ pub struct TokenView {
     pub ln: usize,
     pub cn: usize,
 }
-impl TokenView {
-    fn new(s: &str, ln: usize, cn: usize) -> Self {
-        if let Some(token) = number::lex(s) {
-            Self { token, ln, cn }
+
+fn eat(chars: &mut Peekable<CharIndices>, ln: usize) -> Vec<TokenView> {
+    // skip blank
+    while let Some((_, c)) = chars.peek() {
+        match c {
+            ' ' | '\t' => (),
+            _ => break,
+        }
+        chars.next();
+    }
+
+    // check next
+    let Some(next) = chars.peek() else {
+        return Vec::new();
+    };
+
+    // lex token
+    let cn = next.0 + 1;
+    let mut buf = String::new();
+    while let Some((_, c)) = chars.next() {
+        match c {
+            ' ' | '\t' => break,
+            _ => buf.push(c),
+        }
+    }
+
+    // prepare
+    let mut tokens = Vec::new();
+
+    // pop tail .
+    while !buf.is_empty() {
+        if buf.ends_with(".") {
+            tokens.push(TokenView {
+                token: Token::from(buf.pop().unwrap().to_string()),
+                ln,
+                cn: cn + buf.len(),
+            });
         } else {
-            let token = Token::Symbol(s.to_string());
-            Self { token, ln, cn }
+            break;
         }
     }
 
-    fn dot(ln: usize, cn: usize) -> Self {
-        Self {
-            token: Token::Dot,
+    // finish
+    if !buf.is_empty() {
+        tokens.push(TokenView {
+            token: Token::from(buf),
             ln,
             cn,
-        }
+        });
     }
-}
-
-struct TempToken {
-    s: String,
-    ln: usize,
-    cn: usize,
-}
-impl TempToken {
-    fn new(ln: usize, cn: usize) -> Self {
-        Self {
-            s: String::new(),
-            ln,
-            cn,
-        }
-    }
-}
-
-enum Mode {
-    Blank,
-    Token,
-}
-impl Mode {
-    fn dispatch(
-        &mut self,
-        c: char,
-        ln: usize,
-        cn: usize,
-        token: &mut TempToken,
-        tokens: &mut Vec<TokenView>,
-    ) {
-        match self {
-            Mode::Blank => match c {
-                ' ' | '\t' => (),
-                '.' => tokens.push(TokenView::dot(ln, cn)),
-                _ => {
-                    *token = TempToken::new(ln, cn);
-                    token.s.push(c);
-                    *self = Mode::Token;
-                }
-            },
-            Mode::Token => match c {
-                ' ' | '\t' | '.' => {
-                    tokens.push(TokenView::new(&token.s, token.ln, token.cn));
-                    if c == '.' {
-                        tokens.push(TokenView::dot(ln, cn));
-                    }
-                    *self = Mode::Blank;
-                }
-                _ => token.s.push(c),
-            },
-        }
-    }
-
-    fn dispatch_before_eol(&self, _: &TempToken, _: &mut Vec<TokenView>) {
-        match self {
-            _ => (),
-        }
-    }
-
-    fn dispatch_before_eof(&self, token: &TempToken, tokens: &mut Vec<TokenView>) {
-        match self {
-            Mode::Token => tokens.push(TokenView::new(&token.s, token.ln, token.cn)),
-            _ => (),
-        }
-    }
+    tokens.reverse();
+    tokens
 }
 
 pub fn lex<R: Read>(content: R) -> Result<Vec<TokenView>, EError> {
-    let mut mode = Mode::Blank;
-    let mut token = TempToken::new(0, 0);
     let mut tokens = Vec::new();
     for (ln, l) in BufReader::new(content).lines().enumerate() {
-        for (cn, c) in l?.chars().enumerate() {
-            mode.dispatch(c, ln + 1, cn + 1, &mut token, &mut tokens);
+        let l = l?;
+        let mut chars = l.char_indices().peekable();
+        while chars.peek().is_some() {
+            tokens.append(&mut eat(&mut chars, ln + 1));
         }
-        mode.dispatch_before_eol(&token, &mut tokens);
     }
-    mode.dispatch_before_eof(&token, &mut tokens);
     Ok(tokens)
 }
 
@@ -140,13 +120,52 @@ mod test {
                 TokenView {
                     token: Token::I32(12),
                     ln: 1,
-                    cn: 1
+                    cn: 1,
                 },
                 TokenView {
                     token: Token::Dot,
                     ln: 1,
-                    cn: 3
-                }
+                    cn: 3,
+                },
+            ])
+        );
+    }
+
+    #[test]
+    fn test_lex_foo_dot_minus254i16_dot_dot_bar() {
+        assert_eq!(
+            lex(" foo   .  -254i16..  \n\nbar \n".as_bytes()).unwrap(),
+            Vec::from(&[
+                TokenView {
+                    token: Token::Symbol("foo".to_string()),
+                    ln: 1,
+                    cn: 2,
+                },
+                TokenView {
+                    token: Token::Dot,
+                    ln: 1,
+                    cn: 8,
+                },
+                TokenView {
+                    token: Token::I16(-254),
+                    ln: 1,
+                    cn: 11,
+                },
+                TokenView {
+                    token: Token::Dot,
+                    ln: 1,
+                    cn: 18,
+                },
+                TokenView {
+                    token: Token::Dot,
+                    ln: 1,
+                    cn: 19,
+                },
+                TokenView {
+                    token: Token::Symbol("bar".to_string()),
+                    ln: 3,
+                    cn: 1,
+                },
             ])
         );
     }
