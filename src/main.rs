09efc88c76c18518822054755eb7d8742fb5dd57
Tengu712 <genreihoutengu@gmail.com>
2025-03-29 08:32:13 +0900

    echo line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::error::Error;
+
+type RResult<T> = Result<T, Box<dyn Error>>;
+
+fn run() -> RResult<()> {
+    use std::io::{self, Write};
+
+    loop {
+        print!(">> ");
+        io::stdout().flush()?;
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        // TODO: *exit* @
+        if input.trim() == "*exit*" {
+            break;
+        }
+
+        println!("{input}");
+    }
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: print
+    run().unwrap();
 }
