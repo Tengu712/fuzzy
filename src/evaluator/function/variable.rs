c5552f6c4782ba160e0c8fa2c2fa8e868a200b28
Tengu712 <genreihoutengu@gmail.com>
2025-05-17 23:33:34 +0900

    refactor

--- src/evaluator/function/variable.rs
+++ src/evaluator/function/variable.rs
@@ -4,14 +4,11 @@ pub fn insert_variable_definition(maps: &mut FunctionMap, ty: &str) {
     let map = maps
         .get_mut(ty)
         .unwrap_or_else(|| panic!("unexpected error: function map for '{ty}' not found."));
-    map.insert("->".to_string(), Function::Builtin(define_mutable_variable));
-    map.insert(
-        "=>".to_string(),
-        Function::Builtin(define_immutable_variable),
-    );
+    map.insert("->".to_string(), Function::Builtin(define_mutable));
+    map.insert("=>".to_string(), Function::Builtin(define_immutable));
 }
 
-macro_rules! define_make_variable {
+macro_rules! define_variable_definition {
     ($fn: ident, $name: expr, $mutable: expr) => {
         fn $fn(env: &mut Environment, s: Value, values: &mut Vec<Value>) -> RResult<()> {
             let Some(Value::Symbol(n)) = values.pop() else {
@@ -38,8 +35,8 @@ macro_rules! define_make_variable {
         }
     };
 }
-define_make_variable!(define_mutable_variable, "->", true);
-define_make_variable!(define_immutable_variable, "=>", false);
+define_variable_definition!(define_mutable, "->", true);
+define_variable_definition!(define_immutable, "=>", false);
 
 fn can_define(env: &mut Environment, name: &str) -> bool {
     env.get_variable(name).map(|n| n.mutable).unwrap_or(true)
