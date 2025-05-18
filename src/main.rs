ef6d48db07c5fc97b53c4d8d12bd070e658ac90b
Tengu712 <genreihoutengu@gmail.com>
2025-05-19 08:49:47 +0900

    get args

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,15 @@ use std::error::Error;
 type RResult<T> = Result<T, Box<dyn Error>>;
 
 fn main() {
-    repl::run();
+    use std::env;
+
+    let args = env::args().skip(1).collect::<Vec<String>>();
+
+    if args.is_empty() {
+        repl::run();
+    }
+
+    for n in args {
+        println!("{n}");
+    }
 }
