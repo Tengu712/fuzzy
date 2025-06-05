ee66642b80550f683ea7fa5534a8dcfb93b5b402
Tengu712 <genreihoutengu@gmail.com>
2025-06-05 20:11:14 +0900

    lex self

--- src/lexer.rs
+++ src/lexer.rs
@@ -15,6 +15,7 @@ pub enum Token {
     LBracket,
     RBracket,
     // atoms
+    Slf,
     Top,
     I8(i8),
     U8(u8),
@@ -46,6 +47,7 @@ impl Display for Token {
             Self::RBrace => write!(f, "}}"),
             Self::LBracket => write!(f, "["),
             Self::RBracket => write!(f, "]"),
+            Self::Slf => write!(f, "$"),
             Self::Top => write!(f, "T"),
             n => write!(f, "{n:?}"),
         }
@@ -72,6 +74,8 @@ impl Token {
             Self::LBracket
         } else if s == "]" {
             Self::RBracket
+        } else if s == "$" {
+            Self::Slf
         } else if s == "T" {
             Self::Top
         } else if let Some(n) = parse_number(s) {
@@ -241,6 +245,12 @@ mod test {
     #[test]
     fn test_lex_semicolon() {
         let tokens = lex("1 + 2; * 5").unwrap();
+        insta::assert_yaml_snapshot!(tokens);
+    }
+
+    #[test]
+    fn test_lex_self() {
+        let tokens = lex("1 + $, * $ab").unwrap();
         insta::assert_yaml_snapshot!(tokens);
     }
 
