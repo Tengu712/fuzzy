f91a1ce784d48e175396942d4b2aa516d32b07b3
Tengu712 <genreihoutengu@gmail.com>
2025-06-07 20:31:24 +0900

    refactor

--- src/evaluator/functions/variable.rs
+++ src/evaluator/functions/variable.rs
@@ -10,68 +10,72 @@ pub fn insert(fm: &mut FunctionMapStack, ty: &TypeId) {
     );
 }
 
-macro_rules! define_variable_definition {
-    ($fn: ident, $name: expr, $mutable: expr) => {
-        fn $fn(env: &mut Environment, s: Value, mut args: Vec<Value>) -> RResult<Value> {
-            let o = pop_extract_variant!(args, Symbol);
-            if o == "##" {
-                return Err(format!("error: cannot redefine ##.").into());
-            }
-            if o == "T" {
-                return Err(format!("error: cannot redefine T.").into());
-            }
-
-            // TODO: object
-            // TODO: self
-            if let Some((private, trg_ty, trg)) = split_type_and_name(&o) {
-                // function
-                if let Value::Function((ty, tokens)) = s {
-                    // check if it can redefine?
-                    let trg_ty = TypeId::from(trg_ty);
-                    if env
-                        .fn_map
-                        .get(&trg_ty, trg)
-                        .map(|n| !n.mutable)
-                        .unwrap_or(false)
-                    {
-                        return Err(format!(
-                            "error: cannot redefine {trg} on {trg_ty} because it's immutable."
-                        )
-                        .into());
-                    }
-
-                    // get types
-                    let TypeId::Function(types) = ty else {
-                        panic!("failed to extract function.");
-                    };
-
-                    // insert
-                    let f = Function {
-                        mutable: $mutable,
-                        private,
-                        types,
-                        code: FunctionCode::UserDefined(tokens),
-                    };
-                    env.fn_map
-                        .insert_user_defined(&trg_ty, trg.to_string(), f)?;
-
-                    // finish
-                    return Ok(Value::Nil);
-                }
-            }
-
-            let v = Variable {
-                value: s,
-                mutable: $mutable,
-            };
-            env.vr_map.insert(o, v)?;
-
-            Ok(Value::Nil)
-        }
-    };
+fn define_mutable(env: &mut Environment, s: Value, args: Vec<Value>) -> RResult<Value> {
+    define(env, s, args, "->", true)
 }
-define_variable_definition!(define_mutable, "->", true);
-define_variable_definition!(define_immutable, "=>", false);
+
+fn define_immutable(env: &mut Environment, s: Value, args: Vec<Value>) -> RResult<Value> {
+    define(env, s, args, "=>", false)
+}
+
+fn define(
+    env: &mut Environment,
+    s: Value,
+    mut args: Vec<Value>,
+    arrow: &str,
+    mutable: bool,
+) -> RResult<Value> {
+    let o = pop_extract_variant!(args, Symbol);
+    if o == "##" {
+        return Err("error: cannot redefine ##.".into());
+    }
+    if o == "T" {
+        return Err("error: cannot redefine T.".into());
+    }
+
+    if let Some((private, trg_ty, trg)) = split_type_and_name(&o) {
+        let Value::Function((ty, tokens)) = s else {
+            return Err(format!("error: A : is used in the object of a {arrow}, which is treated as a function definition, but the subject is not a function type.").into());
+        };
+
+        // check if it can redefine?
+        let trg_ty = TypeId::from(trg_ty);
+        if env
+            .fn_map
+            .get(&trg_ty, trg)
+            .map(|n| !n.mutable)
+            .unwrap_or(false)
+        {
+            return Err(format!(
+                "error: cannot redefine {trg} on {trg_ty} because it's immutable."
+            )
+            .into());
+        }
+
+        // get types
+        let TypeId::Function(types) = ty else {
+            panic!("failed to extract function.");
+        };
+
+        // insert
+        let f = Function {
+            mutable,
+            private,
+            types,
+            code: FunctionCode::UserDefined(tokens),
+        };
+        env.fn_map
+            .insert_user_defined(&trg_ty, trg.to_string(), f)?;
+
+        // finish
+        return Ok(Value::Nil);
+    }
+
+    let v = Variable { value: s, mutable };
+    env.vr_map.insert(o, v)?;
+
+    Ok(Value::Nil)
+}
 
 fn split_type_and_name(n: &str) -> Option<(bool, &str, &str)> {
     if let Some((n, m)) = n.split_once("::") {
