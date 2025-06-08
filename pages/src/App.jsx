7e1962dc327c49c884090798882e79be3cdd86b4
Tengu712 <genreihoutengu@gmail.com>
2025-06-08 17:27:36 +0900

    routing

--- pages/src/App.jsx
+++ pages/src/App.jsx
@@ -1,12 +1,31 @@
-import React, { useState } from 'react'
+import { useState, Suspense, lazy } from 'react'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import Layout from './components/Layout'
-import HomePage from './pages/home.mdx'
+
+function MdxPage() {
+  const [hasError, setHasError] = useState(false)
+
+  const filename = useLocation().pathname.slice(7) || 'index'
+  const LazyMdxComponent = lazy(() => import(`./pages/${filename}.mdx`).catch(() => setHasError(true)))
+
+  if (hasError) return <div>Page not found.</div>
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Layout>
+        <LazyMdxComponent />
+      </Layout>
+    </Suspense>
+  )
+}
 
 function App() {
   return (
-    <Layout>
-      <HomePage />
-    </Layout>
+    <Router>
+      <Routes>
+        <Route path='*' element={<MdxPage />} />
+      </Routes>
+    </Router>
   )
 }
 
