ebde71eed49c341341a023065394cf12e30de431
Tengu712 <genreihoutengu@gmail.com>
2025-06-09 03:45:24 +0900

    highlight

--- pages/vite.config.js
+++ pages/vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
+import rehypeShiki from '@shikijs/rehype'
+import fs from 'fs'
+
+const fuzzyGrammar = JSON.parse(fs.readFileSync('./fuzzy.tmLanguage.json', 'utf8'))
 
 export default defineConfig({
   plugins: [
-    mdx(),
+    mdx({
+      rehypePlugins: [
+        [
+          rehypeShiki,
+          {
+            theme: 'github-light',
+            langs: [fuzzyGrammar]
+          }
+        ]
+      ]
+    }),
     react()
   ],
   base: '/fuzzy/'
