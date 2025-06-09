aaffec94cf128a448095770cfb5025bbdc256ee2
Tengu712 <genreihoutengu@gmail.com>
2025-06-09 17:06:01 +0900

    styling

--- pages/vite.config.js
+++ pages/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
         [
           rehypeShiki,
           {
-            theme: 'github-light',
+            theme: 'slack-dark',
             langs: [fuzzyGrammar]
           }
         ]
