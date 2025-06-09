eb19c112f03e1fef7013a8d9f96d7aec2501b678
Tengu712 <genreihoutengu@gmail.com>
2025-06-09 20:39:43 +0900

    add literal&keyword

--- pages/vite.config.js
+++ pages/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
 import rehypeShiki from '@shikijs/rehype'
+import remarkGfm from 'remark-gfm'
 import fs from 'fs'
 
 const fuzzyGrammar = JSON.parse(fs.readFileSync('./fuzzy.tmLanguage.json', 'utf8'))
@@ -9,6 +10,7 @@ const fuzzyGrammar = JSON.parse(fs.readFileSync('./fuzzy.tmLanguage.json', 'utf8
 export default defineConfig({
   plugins: [
     mdx({
+      remarkPlugins: [remarkGfm],
       rehypePlugins: [
         [
           rehypeShiki,
